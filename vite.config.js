ddad19187237116c33b9343153c3fc4b8bfb786e
nihitha14 <2400032980@kluniversity.in>
2026-03-01 18:42:26 +0530

    fix deployment routing

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/FSAD-review1/',   // ✅ EXACT repo name
+  base: '/FSAD-review1/',
   plugins: [react()],
 })
